1e24ba6f764dc9b46a0dda55c163036f1208e5d6
Anna Anderson <anya.anderson.ny@gmail.com>
2021-10-12 21:24:47 -0600

    adding comments to script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function onClick(e) {
   let type = s.options[s.selectedIndex].value;
   let urlEnd = "";
 
-  // check if number is empty
+  // check what the type is
   if (string === "" || type === "random") {
     urlEnd = "random.php";
   }
@@ -28,10 +28,34 @@ function onClick(e) {
           text: "Error calling the Numbers API service: " + response.statusText
         }
       }
-      return response.text();
-    }).then(function(text) {
+      return response.json();
+      //return response.text();
+    //}).then(function(text) {
+    }).then(function(json) {
+      console.log(json); //For informational purposes
+
+      if (string === "" || type === "random") {
+        // update DOM with response
+        //information of interest:
+        //same as with name. I think the output should be the same. Always outputs meal array with one element
+      }
+      else if (type === "name") {
+        // update DOM with response
+        //information of interest:
+        //json.meals[i].strMeal  is the name of the dish
+        //If there is one element (meals[0] only) it returns only that recipe. If not, it returns a list of them. We could either check for this and if there are more than one meal, we could output a list or just output the first entry every time.
+        //json.meals[i].strMealThumb  is a picture of the dish
+        //json.meals[i].strIngredient1  is the begining of the ingredient list. There seems to always be 20 ingredients (strIngredient1, strIngredient2, .., strIngredient20)
+        //json.meals[i].strMeasure1 is the measurements of the ingredients. It seems to be tied to the ingreditent list (also goes to 20) and should be outputted with the ingredients
+        //json.meals[i].strInstructions is one string full of the instructions
+        //Perphaps add json.meals[i].strYoutube which links to youtube
+      }
+      else if (type === "letter"){
+
+      }
       // update DOM with response
-      updateResult(text);
+    //  updateResult(text);
+      updateResult("temp");
     });
 }
 
